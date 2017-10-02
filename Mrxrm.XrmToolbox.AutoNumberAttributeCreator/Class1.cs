3dc5a5d734a31a5a926f1cc8baebf45e80521deb
Kelvin <kelvin_shen@hotmail.com>
2017-10-03 06:44:10 +1300

    Add project files.

---
+++ Mrxrm.XrmToolbox.AutoNumberAttributeCreator/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mrxrm.XrmToolbox.AutoNumberAttributeCreator
+{
+    public class Class1
+    {
+    }
+}
